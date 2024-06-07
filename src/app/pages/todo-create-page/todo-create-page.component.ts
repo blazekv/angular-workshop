10ee32a6309479b0418770ba076082e569f6c72c
Vladimir Blazek <vladimir.blazek@quanti.cz>
2024-06-07 14:57:47 +0200

    Remove Template driven form

--- src/app/pages/todo-create-page/todo-create-page.component.ts
+++ src/app/pages/todo-create-page/todo-create-page.component.ts
@@ -1,12 +1,11 @@
 import { Component } from '@angular/core';
 import { TodoFormReactiveComponent } from '../../components/todo-form-reactive/todo-form-reactive.component';
 import { Todo } from '../../model/Todo';
-import { TodoFormTemplateComponent } from '../../components/todo-form-template/todo-form-template.component';
 
 @Component({
   selector: 'eg-todo-create-page',
   standalone: true,
-  imports: [TodoFormReactiveComponent, TodoFormTemplateComponent],
+  imports: [TodoFormReactiveComponent],
   templateUrl: './todo-create-page.component.html',
   styleUrl: './todo-create-page.component.scss',
 })
